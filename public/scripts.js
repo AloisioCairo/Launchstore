fff7a36bea802a85584fa5588103e1631daf8d1a
ALOISIO SAMPAIO CAIRO <aloisiocairo@gmail.com>
2021-03-22 07:07:06 -0300

    Implementado a visualização/funcionamento do lightbox de produtos

--- public/scripts.js
+++ public/scripts.js
@@ -143,3 +143,37 @@ const PhotosUpload = {
         photoDiv.remove()
     }
 }
+
+// Aula: Fase 04 - Upload de imagens - Galeria de imagens - Funcionalidade da galeria de imagem
+const ImageGallery = {
+    highlight: document.querySelector('.gallery .highlight > img'),
+    previews: document.querySelectorAll('.gallery-preview img'),
+    setImage(e) {
+        const { target } = e
+
+        ImageGallery.previews.forEach(preview => preview.classList.remove('active'))
+        target.classList.add('active')
+
+        ImageGallery.highlight.src = target.src
+        Lightbox.image.src = target.src
+    }
+}
+
+// Aula: Fase 04 - Upload de imagens - Galeria de imagens - Javascript do Lightbox
+const Lightbox = {
+    target: document.querySelector('.lightbox-target'),
+    image: document.querySelector('.lightbox-target img'),
+    closeButton: document.querySelector('.lightbox a.lightbox-close'),
+    open() {
+        Lightbox.target.style.opacity = 1
+        Lightbox.target.style.top = 0
+        Lightbox.target.style.botton = 0
+        Lightbox.closeButton.style.top = 0
+    },
+    close() {
+        Lightbox.target.style.opacity = 0
+        Lightbox.target.style.top = "-100%"
+        Lightbox.target.style.botton = "initial"
+        Lightbox.closeButton.style.top = "-80px"
+    }
+}
