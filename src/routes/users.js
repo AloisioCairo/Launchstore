b0c575c3e62cb5454e786161347bc3132640391c
ALOISIO SAMPAIO CAIRO <aloisiocairo@gmail.com>
2021-04-15 21:07:35 -0300

    Implementado regra de acesso ao cadastro de produtos

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,7 +4,7 @@ const SessionController = require('../app/controllers/SessionController')
 const UserController = require('../app/controllers/UserController')
 const UserValidator = require('../app/validators/user')
 const SessionValidator = require('../app/validators/session')
-const { isLoggedRedirectToUsers } = require('../app/middlewares/session')
+const { isLoggedRedirectToUsers, onlyUsers } = require('../app/middlewares/session')
 
 // // login/logout
 routes.get('/login', isLoggedRedirectToUsers, SessionController.loginForm)
@@ -21,7 +21,7 @@ routes.post('/logout', SessionController.logout)
 routes.get('/register', UserController.registerForm)
 routes.post('/register', UserValidator.post, UserController.post)
 
-routes.get('/', UserValidator.show, UserController.show)
+routes.get('/', onlyUsers, UserValidator.show, UserController.show)
 routes.put('/', UserValidator.update, UserController.update)
 // routes.delete('/', UserController.delete)
 
